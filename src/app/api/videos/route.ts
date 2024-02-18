4f06ef573456ce97459adc8ce6fc34517bb80946
Shivam Maheshwari <shivammaheshwari503@gmail.com>
2024-02-18 15:02:09 +0530

    changes

--- src/app/api/videos/route.ts
+++ src/app/api/videos/route.ts
@@ -12,34 +12,34 @@ export interface Videos {
     sources: string[];
 }
 
-
 export async function GET(req: NextRequest, res: NextResponse) {
     try {
         const existingData: Videos[] = JSON.parse(fs.readFileSync('data/data.json', 'utf-8'));
-        existingData.sort((a, b) => a.order - b.order)
+        existingData.sort((a, b) => a.order - b.order);
         return NextResponse.json(existingData);
     } catch (error) {
-        console.error('Error saving Video data:', error);
-        NextResponse.json({ success: false, message: 'Failed to get Video data' });
+        console.error('Error fetching Video data:', error);
+        return NextResponse.json({ success: false, message: 'Failed to fetch Video data' });
     }
-};
-
+}
 
 export async function PUT(req: NextRequest, res: NextResponse) {
     try {
         const newData: Videos[] = await req.json();
-        const existingData: Videos[] = JSON.parse(fs.readFileSync('data/data.json', 'utf-8'));
-       for(let val of newData){
-        const index = existingData.findIndex(item => item.id == val.id);
-        if (index === -1) {
-            return NextResponse.json({ success: false, message: 'video data not found' });
+        let existingData: Videos[] = JSON.parse(fs.readFileSync('data/data.json', 'utf-8'));
+
+        for (let val of newData) {
+            const index = existingData.findIndex(item => item.id === val.id);
+            if (index === -1) {
+                return NextResponse.json({ success: false, message: 'Video data not found' });
+            }
+            existingData[index] = { ...existingData[index], ...val };
         }
-        existingData[index] = { ...existingData[index], ...val };
+
         fs.writeFileSync('data/data.json', JSON.stringify(existingData, null, 2));
-      }
-        return NextResponse.json({ data: existingData, message: 'video data updated successfully' });
+        return NextResponse.json({ data: existingData, message: 'Video data updated successfully' });
     } catch (error) {
         console.error('Error saving video data:', error);
-        NextResponse.json({ success: false, message: 'Failed to get video data' });
+        return NextResponse.json({ success: false, message: 'Failed to update video data' });
     }
-};
+}
